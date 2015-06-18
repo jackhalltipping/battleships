c20d71b0a035960b8740e2d0b6b7dad23062e345
Zeina Dajani <dajani.zeina@gmail.com>
2015-06-18 16:47:19 +0100

    trying to add multiple locations

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,36 @@
+require_relative 'board'
+
 class Player
 
   attr_reader :board, :hits, :misses
 
   def initialize
-    @board = []
+    @board = Board.new
     @hits = 0
     @misses = 0
   end
 
+
+  #
+  # def ship_position ship
+  #   board.
+  # end
+
+  def place_destroyer position, orientation
+    positions = orientation
+    self.place_on_board((destroyer = Ship.new 5), positions)
+  end
+
+  def place_sailboat position
+    self.give_position((sailboat = Ship.new 2), position)
+  end
+
+  private
+
+  def get_positions ship
+  end
+
+  def place_on_board ship, positions
+    board.accept_ship ship, positions
+  end
 end
--- lib/temp.rb
+++ lib/temp.rb
@@ -1,12 +1,8 @@
-class Temp
+class FindCoords
 
-  def letter_coords
-    letters_coords = [:A..:J]
-  end
+  LETTER_COORDS = ("A".."J").to_a
+  NUMBER_COORDS = ("1".."10").to_a
 
-  def number_coords
-    number_coords = [1..10]
-  end
 attr_accessor :grid
 
 def initialize
@@ -22,9 +18,50 @@ end
   end
 
   def creating_board2
-    board = ('A'..'J').zip(1..10).to_h
+    board = ('A'..'J').zip(1..10).to_s
   end
+
+  def next_number letter, number
+    # letter = letter
+    # number = number
+    letter + (number + 1)
+    grid[("#{letter}#{number}").to_sym] = 'ship'
+  end
+
+  def place ship, coordinate, orientation
+    y = coordinate[0]
+    x = coodrinate[1].to_i
+    x_coordinate_range = (x...(x + ship.size)).to_a
+
+    x_coordinate_range.each do |x_coordinate|
+      x_and_y = (y + x_coordinate.to_s).to_sym
+      grid[x_and_y] = ship
+    end
+  end
+
+  def coord_finder
+
+  end
+
+  def gets_west size, position
+    positions = [position]
+    (size - 1).times {positions << positions.last.next }
+    positions
+  end
+
+
+  def gets_south size, position
+
+  end
+
+  def self.split position
+    @l = LETTER_COORDS.find_index(position[0])
+    @n = NUMBER_COORDS.find_index(position[1])
+  end
+
 end
+
+
 
 # number_coords = [1..10]
 # coords_together = letters_coords.product(number_coords)
